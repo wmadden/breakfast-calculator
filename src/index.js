3977bb6d0f6725784ecf1c9faa73170bc383305d
Will Madden <w.a.madden@gmail.com>
2017-03-26 19:15:31 +0200

    (refactor) Extract meal plan calculation from printing code

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ const { getFoodData } = require('./getFoodData');
 const { Solver } = require('./Solver');
 const { PROTEIN, CARBOHYDRATES, FAT } = require('./constants');
 
-const desiredProtein = 100;
-const desiredCarbohydrates = 100;
-const desiredFat = 100;
+function generateMealPlan({ availableFoods, desiredNutrients }) {
+  const {
+    desiredProtein, desiredCarbohydrates, desiredFat,
+  } = desiredNutrients;
 
-const desiredFoods = ['eggs', 'protein powder', 'oats', 'vanilla soy yoghurt', 'banana'];
-
-getFoodData().then((foodData) => {
-  const availableFoods = foodData.filter(food => desiredFoods.includes(food.name));
   const { foodVariables } = Solver({
     availableFoods,
     desiredProtein,
@@ -18,15 +15,8 @@ getFoodData().then((foodData) => {
     desiredFat,
   });
 
-  console.log('');
-  console.log('Desired protein:', desiredProtein);
-  console.log('Desired carbohydrates:', desiredCarbohydrates);
-  console.log('Desired fat:', desiredFat);
-  console.log('');
-  console.log('Meal plan: ingredient amount (g) (protein, carbohydrates, fat)');
-  console.log('');
-
   const totals = { [PROTEIN]: 0, [CARBOHYDRATES]: 0, [FAT]: 0 };
+  const ingredients = [];
 
   foodVariables.forEach(({ foodDescriptor, variable }) => {
     const { name, protein, carbohydrates, fat } = foodDescriptor;
@@ -40,12 +30,58 @@ getFoodData().then((foodData) => {
     totals[CARBOHYDRATES] += amountCarbohydrates;
     totals[FAT] += amountFat;
 
-    console.log(`${name}: ${amount.toFixed(0)}g (${amountProtein.toFixed(1)}, ${amountCarbohydrates.toFixed(1)}, ${amountFat.toFixed(1)})`);
+    ingredients.push({
+      name,
+      amount,
+      foodDescriptor,
+      [PROTEIN]: amountProtein,
+      [CARBOHYDRATES]: amountCarbohydrates,
+      [FAT]: amountFat,
+    });
   });
 
-  console.log('');
-  console.log('Total:');
-  console.log('  Protein:      ', totals[PROTEIN].toFixed(1));
-  console.log('  Carbohydrates:', totals[CARBOHYDRATES].toFixed(1));
-  console.log('  Fat:          ', totals[FAT].toFixed(1));
-});
+  return {
+    totals,
+    ingredients,
+  };
+}
+
+function main() {
+  const desiredProtein = 100;
+  const desiredCarbohydrates = 100;
+  const desiredFat = 100;
+
+  const desiredFoods = ['eggs', 'protein powder', 'oats', 'vanilla soy yoghurt', 'banana'];
+
+  getFoodData().then((foodData) => {
+    const availableFoods = foodData.filter(food => desiredFoods.includes(food.name));
+    const { ingredients, totals } = generateMealPlan({
+      availableFoods,
+      desiredNutrients: {
+        desiredProtein,
+        desiredCarbohydrates,
+        desiredFat,
+      },
+    });
+
+    console.log('');
+    console.log('Desired protein:', desiredProtein);
+    console.log('Desired carbohydrates:', desiredCarbohydrates);
+    console.log('Desired fat:', desiredFat);
+    console.log('');
+    console.log('Meal plan: ingredient amount (g) (protein, carbohydrates, fat)');
+    console.log('');
+
+    ingredients.forEach(({ name, amount, protein, carbohydrates, fat }) => {
+      console.log(`${name}: ${amount.toFixed(0)}g (${protein.toFixed(1)}, ${carbohydrates.toFixed(1)}, ${fat.toFixed(1)})`);
+    });
+
+    console.log('');
+    console.log('Total:');
+    console.log('  Protein:      ', totals[PROTEIN].toFixed(1));
+    console.log('  Carbohydrates:', totals[CARBOHYDRATES].toFixed(1));
+    console.log('  Fat:          ', totals[FAT].toFixed(1));
+  });
+}
+
+main();
