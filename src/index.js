05c8674a1dc153b0dfd2352b5e7349468ffed9df
Will Madden <w.a.madden@gmail.com>
2017-03-27 12:44:01 +0200

    (feat) Make printed output more readable

--- src/index.js
+++ src/index.js
@@ -35,18 +35,29 @@ function main() {
     console.log('Desired carbohydrates:', desiredCarbohydrates);
     console.log('Desired fat:', desiredFat);
     console.log('');
+    console.log('Preset amounts:');
+    availableFoods.forEach((ingredient) => {
+      const constraints = ingredientConstraints[ingredient.name];
+      if (!constraints) return;
+      console.log(`  ${ingredient.name}:`);
+      if (constraints.hasOwnProperty('min')) console.log(`    minimum: ${constraints.min}g`);
+      if (constraints.hasOwnProperty('equal')) console.log(`    exactly: ${constraints.equal}g`);
+      if (constraints.hasOwnProperty('max')) console.log(`    maximum: ${constraints.max}g`);
+    });
+    console.log('');
     console.log('Meal plan: ingredient amount (g) (protein, carbohydrates, fat)');
     console.log('');
 
     ingredients.forEach(({ foodDescriptor, amount, nutrientAmounts }) => {
-      console.log(`${foodDescriptor.name}: ${amount.toFixed(0)}g (${nutrientAmounts.protein.toFixed(1)}, ${nutrientAmounts.carbohydrates.toFixed(1)}, ${nutrientAmounts.fat.toFixed(1)})`);
+      console.log(`  ${foodDescriptor.name}: ${amount.toFixed(0)}g`);
+      console.log(`    (${nutrientAmounts.protein.toFixed(1)}g, ${nutrientAmounts.carbohydrates.toFixed(1)}g, ${nutrientAmounts.fat.toFixed(1)}g)`);
     });
 
     console.log('');
     console.log('Total:');
-    console.log('  Protein:      ', totals.protein.toFixed(1));
-    console.log('  Carbohydrates:', totals.carbohydrates.toFixed(1));
-    console.log('  Fat:          ', totals.fat.toFixed(1));
+    console.log(`  Protein:       ${totals.protein.toFixed(1)}g (${(totals.protein / desiredProtein * 100).toFixed(1)}%)`);
+    console.log(`  Carbohydrates: ${totals.carbohydrates.toFixed(1)}g (${(totals.carbohydrates / desiredCarbohydrates * 100).toFixed(1)}%)`);
+    console.log(`  Fat:           ${totals.fat.toFixed(1)}g (${(totals.fat / desiredFat * 100).toFixed(1)}%)`);
   });
 }
 
